6bdc250f5bd74ec85f8853ae32f83a9d61e127ac
Gustavo Borges <srcenouraa@gmail.com>
2022-12-23 18:18:04 -0300

    Revert "first commit"
    This reverts commit 1cdbacf658a5e54af2829449850c9cc9f6c4c49d.

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbHost = 'localhost';
-$dbport = 5432;
-$dbUsername = 'postgres';
-$dbPassword = 'postgres';
-$dbName = 'formulario-gustavo';
-
-$myPDO = new PDO("pgsql:host=$dbHost; dbname=$dbName", "postgres", "postgres");
